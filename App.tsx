079cb3e293936ed4e48fc3e07ddf4f99706f063e
Arvind Debnath <80598358+Suptohita@users.noreply.github.com>
2022-08-16 23:52:24 +0600

    Initialize project

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle="light-content" hidden />
+      <View style={styles.container}>
+        <Text>This is React native bottom sheet</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%'
+  }
+});
+
+export default App;
